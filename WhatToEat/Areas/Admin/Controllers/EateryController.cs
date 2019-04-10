88f73eea43b740552deaac082eaf162091337b7a
Niall <niallmcclelland166@hotmail.com>
2019-04-10 22:32:03 +0100

    Creare Eatery Section Part 2

--- WhatToEat/Areas/Admin/Controllers/EateryController.cs
+++ WhatToEat/Areas/Admin/Controllers/EateryController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WhatToEat.Models.Data;
+using WhatToEat.Models.ViewModels.Eatery;
 
 namespace WhatToEat.Areas.Admin.Controllers
 {
     public class EateryController : Controller
     {
-        // GET: Admin/Eatery
-        public ActionResult Index()
+        // GET: Admin/Eatery/categories
+        public ActionResult categories()
         {
-            return View();
+            // Declare a list of models
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+                // Init the list
+                categoryVMList = db.Categories
+                .ToArray()
+                .OrderBy(x => x.Sorting)
+                .Select(x => new CategoryVM(x))
+                .ToList();
+            }
+
+            // Return view to list
+            return View(categoryVMList);
         }
     }
 }
--- WhatToEat/Models/Data/Db.cs
+++ WhatToEat/Models/Data/Db.cs
@@ -10,6 +10,8 @@ namespace WhatToEat.Models.Data
     {
         public DbSet <PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
-        public DbSet<SidebarDTO> Categories { get; set; }
+        public DbSet<CategoryDTO> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<WhatToEat.Models.ViewModels.Eatery.CategoryVM> CategoryVMs { get; set; }
     }
 }
