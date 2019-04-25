526d1547eb33a247daa24dc0623959194f7e2cd7
Niall <niallmcclelland166@hotmail.com>
2019-04-25 22:05:24 +0100

    Create Eatery Section Part 14 - Upload Pictures and Image Preview (34)

--- WhatToEat/Areas/Admin/Controllers/EateryController.cs
+++ WhatToEat/Areas/Admin/Controllers/EateryController.cs
@@ -407,6 +407,73 @@ namespace WhatToEat.Areas.Admin.Controllers
 
                 #region Image Uplaod
 
+                // Check for file upload
+                if (file != null && file.ContentLength > 0) {
+
+                // Get extension 
+                string ext = file.ContentType.ToLower();
+
+                // Verify extension
+                if (ext != "image/jpg" &&
+                ext != "image/jpeg" &&
+                ext != "image/pjpeg" &&
+                ext != "image/gif" &&
+                ext != "image/x-png" &&
+                ext != "image/png")
+                {
+                    using (Db db = new Db())
+
+                    {
+                        ModelState.AddModelError("", "The image was not uploaded - wrong image extension.");
+                        return View(model);
+                    }
+
+                }
+
+                //Set upload directory paths
+                var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+
+                var pathString1 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString());
+                var pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Thumbs");
+
+                //Delete files from directory paths
+
+                DirectoryInfo di1 = new DirectoryInfo(pathString1);
+                DirectoryInfo di2 = new DirectoryInfo(pathString2);
+
+                foreach (FileInfo file2 in di1.GetFiles())
+                    file2.Delete();
+
+                foreach (FileInfo file3 in di1.GetFiles())
+                    file3.Delete();
+
+                //Save image name
+                string imageName = file.FileName;
+
+                using (Db db = new Db())
+                {
+                    ProductDTO dto = db.Products.Find(id);
+                    dto.ImageName = imageName;
+
+                    db.SaveChanges();
+
+                }
+
+                //Save original and thumb images
+
+                var path = string.Format("{0}\\{1}", pathString1, imageName);
+                var path2 = string.Format("{0}\\{1}", pathString2, imageName);
+
+                // Save original
+                file.SaveAs(path);
+
+                // Create and save thumb
+                WebImage img = new WebImage(file.InputStream);
+                img.Resize(200, 200);
+                img.Save(path2);
+
+            }
+
                 #endregion
 
             // Redirect
