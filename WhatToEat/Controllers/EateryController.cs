865ece0a0f0734fed32aa1495c934e6107c17790
Niall <niallmcclelland166@hotmail.com>
2019-05-01 22:53:49 +0100

    Eatery Front End Part 2 - Italian and Healthy category link not working (43)

--- WhatToEat/Controllers/EateryController.cs
+++ WhatToEat/Controllers/EateryController.cs
@@ -12,9 +12,9 @@ namespace WhatToEat.Controllers
     {
         // GET: Eatery
         public ActionResult Index()
-        
+
         {
-           return RedirectToAction("Index", "Pages");
+            return RedirectToAction("Index", "Pages");
         }
 
         public ActionResult CategoryMenuPartial()
@@ -31,5 +31,42 @@ namespace WhatToEat.Controllers
             //Return partial with list
             return PartialView(categoryVMList);
         }
+        
+        //Get: /eatery/category/name
+        public ActionResult Category(string name)
+        {
+            //Declare a list of ProductVM
+            List<ProductVM> productVMList;
+
+            using (Db db = new Db())
+            {
+                //Get category id
+                CategoryDTO categoryDTO = db.Categories.Where(x => x.Slug == name).FirstOrDefault();
+                int catId = categoryDTO.Id;
+
+                //Init the list
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId).Select(x => new ProductVM(x)).ToList();
+
+                //Get category name
+                var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+                ViewBag.CategoryName = productCat.CategoryName;
+
+            }
+
+            //Return view with list
+            return View(productVMList);
+
+
+
+
+
+
+
+
+
+
+
+        }
+
     }
 }
