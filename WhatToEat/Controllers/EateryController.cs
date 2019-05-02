7ed9e789825c1e3b7147448de9aa0e250330cba7
Niall <niallmcclelland166@hotmail.com>
2019-05-02 20:27:40 +0100

    Eatery Front End Part 3 - Images in Products Not Showing Path Might Have Changed (44)

--- WhatToEat/Controllers/EateryController.cs
+++ WhatToEat/Controllers/EateryController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -68,5 +69,42 @@ namespace WhatToEat.Controllers
 
         }
 
+        [ActionName("product-details")]
+        public ActionResult ProductDetails(string name)
+        {
+            //Declare the VM and DTO
+            ProductVM model;
+            ProductDTO dto;
+
+            //Init product id
+            int id = 0;
+
+            using (Db db = new Db())
+            {
+              
+                 //Check if product exists
+                 if (! db.Products.Any(x => x.Slug.Equals(name)))
+                    {
+                        return RedirectToAction("Index", "Eatery");
+                    }
+
+                    //Init productDTO
+                    dto = db.Products.Where(x => x.Slug == name).FirstOrDefault();
+
+                    //Get id
+                    id = dto.Id;
+
+                    //Init model
+                    model = new ProductVM(dto);
+
+                }
+
+            // Get gallery images
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products" + id + "/Gallery/Thumbs"))
+            .Select(fn => Path.GetFileName(fn));
+
+                //Return view with model
+                return View("ProductDetails", model);
+            }
+        }
     }
-}
